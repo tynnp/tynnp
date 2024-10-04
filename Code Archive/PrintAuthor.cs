0d2d4c7e328c1a600c60bf9c3b607b2d15612bef
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-10-04 14:18:11 +0700

    Hàm PrintAuthor

---
+++ Code Archive/PrintAuthor.cs
@@ -0,0 +1,18 @@
+static void PrintAuthor() {
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine();
+
+    Console.WriteLine("""
+
+        ████████╗██╗   ██╗███╗   ██╗███╗   ██╗██████╗ 
+        ╚══██╔══╝╚██╗ ██╔╝████╗  ██║████╗  ██║██╔══██╗
+           ██║    ╚████╔╝ ██╔██╗ ██║██╔██╗ ██║██████╔╝
+           ██║     ╚██╔╝  ██║╚██╗██║██║╚██╗██║██╔═══╝ 
+           ██║      ██║   ██║ ╚████║██║ ╚████║██║     
+           ╚═╝      ╚═╝   ╚═╝  ╚═══╝╚═╝  ╚═══╝╚═╝   program   
+        ----------------------------------------------------
+
+        """);
+            
+    Console.ForegroundColor = ConsoleColor.White;
+}
