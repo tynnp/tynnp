00509b3b9ed5ee9e059caa830a44d73a8a5a66d8
Nguyễn Ngọc Phú Tỷ <148174474+tynnp@users.noreply.github.com>
2024-10-23 09:34:29 +0700

    tool check copy code, so sánh độ tương đồng

--- Compare_code/Compare_code.py
+++ Compare_code/Compare_code.py
@@ -3,34 +3,40 @@ from tkinter import filedialog, messagebox
 from difflib import SequenceMatcher
 from tkinter import scrolledtext
 
+# Bỏ các dòng trống và khoảng trắng trong file code
 def filter_code_lines(lines):
     return [line.strip() for line in lines if line.strip()]
 
+# Hàm đọc nội dung file 
 def read_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         return file.readlines()
 
+# Tính toán độ tương đồng 
 def calculate_similarity(text1, text2):
     return SequenceMatcher(None, text1, text2).ratio()
 
+# So sánh nội dung hai file, trả về tỉ lệ tương đồng
 def compare_files(file1_lines, file2_lines):
     filtered_file1 = filter_code_lines(file1_lines)
     filtered_file2 = filter_code_lines(file2_lines)
     similarity_ratio = calculate_similarity("\n".join(filtered_file1), "\n".join(filtered_file2))
     return similarity_ratio
 
+# Hàm mở hộp thoại chọn file 
 def open_file():
     file_path = filedialog.askopenfilename(title="Chọn file", filetypes=(("Code files", "*.py;*.cpp;*.txt"), ("All files", "*.*")))
     return file_path
 
+# Hàm tô màu các đoạn code giống nhau 
 def color_code(file_text, file1_lines, file2_lines):
-    file_text.delete(1.0, tk.END)
+    file_text.delete(1.0, tk.END) 
     matcher = SequenceMatcher(None, file1_lines, file2_lines)
     
     for tag, i1, i2, j1, j2 in matcher.get_opcodes():
         text1 = ''.join(file1_lines[i1:i2])
         
-        if tag == "equal":
+        if tag == "equal":  
             file_text.insert(tk.END, text1, "match")
         else:
             file_text.insert(tk.END, text1)
@@ -46,14 +52,19 @@ def compare_code():
     file1_lines = read_file(file1_path)
     file2_lines = read_file(file2_path)
     similarity_ratio = compare_files(file1_lines, file2_lines)
+
     color_code(file1_text, file1_lines, file2_lines)
     color_code(file2_text, file2_lines, file1_lines)
+
+    # Hiển thị tỉ lệ tương đồng
     similarity_percentage = similarity_ratio * 100
     similarity_text = f"Độ tương đồng: "
     percentage_text = f"{similarity_percentage:.2f}%"
 
     result_label.config(text=similarity_text, fg="black", font=("Arial", 12, "bold"))
     percentage_label.config(text=percentage_text, font=("Arial", 12, "bold"))
+
+    # Đổi màu tỉ lệ tương đồng dựa trên ngưỡng 70%
     if similarity_percentage >= 70:
         percentage_label.config(fg="red")
     else:
